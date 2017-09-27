d0465d9db8a3426fa9af6ef5981fca7be259c749
Krasi <krasi2405@gmail.com>
2017-09-28 01:58:54 +0300

    Added camera scripts for movement and keeping in bounds

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    
+
+    [SerializeField]
+    public float cameraBaseMoveSpeed = 0.1f;
+    
+    [SerializeField]
+    public float cameraScaleSpeed = 0.05f;
+
+    [SerializeField]
+    public float cameraMaxSize = 25f;
+    [SerializeField]
+    public float cameraMinSize = 1f;
+
+    private Camera camera;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+
+    void Update() {
+
+        if (IsShiftClicked())
+        {
+            
+
+            if (Input.GetKey(KeyCode.W) && camera.orthographicSize < cameraMaxSize)
+            {
+                camera.orthographicSize += cameraScaleSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.S) && camera.orthographicSize > cameraMinSize)
+            {
+                camera.orthographicSize -= cameraScaleSpeed;
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(new Vector3(0, GetCameraMoveSpeed(), 0));
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(new Vector3(0, -GetCameraMoveSpeed(), 0));
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(new Vector3(-GetCameraMoveSpeed(), 0, 0));
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(new Vector3(GetCameraMoveSpeed(), 0, 0));
+            }
+        }
+    }
+
+
+
+    public bool IsShiftClicked()
+    {
+        return Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+    }
+
+    public float GetCameraMoveSpeed()
+    {
+        return cameraBaseMoveSpeed * camera.orthographicSize / 5;
+    }
+}
---
+++ Assets/Scripts/KeepInBounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Created with help from
+// http://answers.unity3d.com/questions/501893/calculating-2d-camera-bounds.html
+public class KeepInBounds : MonoBehaviour {
+
+    [SerializeField]
+    private float x;
+    [SerializeField]
+    private float y;
+
+
+
+    private void LateUpdate() {
+        Vector3 position = transform.position;
+        float cameraSize = GetComponent<Camera>().orthographicSize;
+        position.x = Mathf.Clamp(
+            position.x,
+            -x + cameraSize * Screen.width / Screen.height,
+            x - cameraSize * Screen.width / Screen.height);
+        position.y = Mathf.Clamp(
+            position.y,
+            -y + cameraSize,
+            y - cameraSize);
+
+        transform.position = position;
+    }
+}
