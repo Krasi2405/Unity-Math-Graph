4d5f6b99ec50c4a9f7352f96dc817f8a156b379b
Krasi <krasi2405@gmail.com>
2017-09-27 23:54:02 +0300

    Added automatic creation of grid, basic nonchangeable formula display

---
+++ Assets/Scripts/CreateMap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMap : MonoBehaviour {
+
+    public GameObject verticalLine;
+    public GameObject horizontalLine;
+    public GameObject dot;
+
+	void Start () {
+        for (int i = -20; i <= 20; i++)
+        {
+            for (int j = -20; j <= 20; j++)
+            {
+                Instantiate(verticalLine, new Vector3(i, j, 0), Quaternion.identity);
+                Instantiate(horizontalLine, new Vector3(i, j, 0), Quaternion.identity);
+                Instantiate(dot, new Vector3(i, j, 0), Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DisplayGraph.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NCalc;
+
+public class DisplayGraph : MonoBehaviour {
+
+    public GameObject dot;
+    public float valueStart;
+    public float valueEnd;
+    public float valueStep;
+    public string mathFunction;
+
+	void Start () {
+        for (float i = valueStart; i <= valueEnd; i += valueStep)
+        {
+            Expression e = new Expression("Pow([value], 2)");
+            e.Parameters["value"] = i;
+            float value = float.Parse(e.Evaluate().ToString());
+            Instantiate(dot, new Vector3(i, value, 0), Quaternion.identity);
+        }
+    }
+}
