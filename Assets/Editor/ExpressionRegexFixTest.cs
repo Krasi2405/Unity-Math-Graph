c945a59a08bacda3f81e21aab2407cb31db70c0d
Krasi <krasi2405@gmail.com>
2017-10-03 03:56:58 +0300

    Added a way for the user to add unfiltered math expressions that turn into NCalc compatible expressions.

---
+++ Assets/Editor/ExpressionRegexFixTest.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class ExpressionRegexFixTest {
+
+    private ExpressionHumanizer humanizer;
+
+    [SetUp]
+    public void SetUp()
+    {
+        humanizer = new ExpressionHumanizer();
+    }
+
+	[Test]
+	public void T00ReturnsUnchangedExpression() {
+        string expressionHuman = "2 + x";
+        string expressionNCalc = "2 + x";
+		Assert.AreEqual(expressionNCalc, humanizer.Humanize(expressionHuman));
+	}
+
+
+    [Test]
+    public void T01HandleMultiplication()
+    {
+        string expressionHuman = "2x";
+        string expressionNCalc = "2*x";
+        Assert.AreEqual(expressionNCalc, humanizer.Humanize(expressionHuman));
+    }
+
+
+    [Test]
+    public void T02HandleMultipleMultiplications()
+    {
+        string expressionHuman = "2x + 4x + 20y";
+        string expressionNCalc = "2*x + 4*x + 20*y";
+        Assert.AreEqual(expressionNCalc, humanizer.Humanize(expressionHuman));
+    }
+
+    [Test]
+    public void T03HandleSquaringNumber()
+    {
+        string expressionHuman = "x^2";
+        string expressionNCalc = "Pow(x,2)";
+        Assert.AreEqual(expressionNCalc, humanizer.Humanize(expressionHuman));
+    }
+
+    [Test]
+    public void T04HandleMultipleSquaring()
+    {
+        string expressionHuman = "x^2 + 2^y";
+        string expressionNCalc = "Pow(x,2) + Pow(2,y)";
+        Assert.AreEqual(expressionNCalc, humanizer.Humanize(expressionHuman));
+    }
+
+    [Test]
+    public void T05HandleAbsoluteValues()
+    {
+        string expressionHuman = "|2x + 5|";
+        string expressionNCalc = "Abs(2*x + 5)";
+        Assert.AreEqual(expressionNCalc, humanizer.Humanize(expressionHuman));
+    }
+
+    // FULL RUNS
+    [Test]
+    public void T06HandleAll00()
+    {
+        string expressionHuman = "|4x + y^2| + 5";
+        string expressionNCalc = "Abs(4*x + Pow(y,2)) + 5";
+        Assert.AreEqual(expressionNCalc, humanizer.Humanize(expressionHuman));
+    }
+
+    [Test]
+    public void T06HandleAll01()
+    {
+        string expressionHuman = "|2 - |4x + y^2| + 5|";
+        string expressionNCalc = "Abs(2 - Abs(4*x + Pow(y,2)) + 5)";
+        Assert.AreEqual(expressionNCalc, humanizer.Humanize(expressionHuman));
+    }
+
+    [Test]
+    public void T07HandleAll02()
+    {
+        string expressionHuman = "|2^3 + y^x + 2x|";
+        string expressionNCalc = "Abs(Pow(2,3) + Pow(y,x) + 2*x)";
+        Assert.AreEqual(expressionNCalc, humanizer.Humanize(expressionHuman));
+    }
+}
---
+++ Assets/Scripts/ExpressionHumanizer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class ExpressionHumanizer {
+
+	
+    public string Humanize(string expression)
+    {
+        expression = HandleMultiplication(expression);
+        expression = HandleSquaring(expression);
+        expression = HandleAbsoluteValue(expression);
+        return expression;
+    }
+
+
+    private string HandleMultiplication(string expression)
+    {
+        MatchCollection matchCollection = Regex.Matches(expression, @"(\d+)(\w+)");
+        foreach(Match match in matchCollection)
+        {   
+            expression = expression.Replace(
+                match.ToString(), 
+                match.Groups[1].ToString() + "*" + match.Groups[2]);
+        }
+        return expression;
+    }
+
+
+    private string HandleSquaring(string expression)
+    {
+        MatchCollection matchCollection = Regex.Matches(
+            expression,
+            // Handle any sort of combination between words and numbers.
+            @"((\w+)\^(\w+))|((\d+)\^(\w+))|((\w+)\^(\d+))|((\d+)\^(\d+))");
+        
+
+        foreach (Match match in matchCollection)
+        {
+            expression = expression.Replace(
+                match.ToString(),
+                "Pow(" + match.Groups[2].ToString() + "," + match.Groups[3] + ")");
+        }
+        return expression;
+    }
+
+
+    private string HandleAbsoluteValue(string expression)
+    {
+        MatchCollection matchCollection = Regex.Matches(
+            expression,
+            @"\|(.+)\|");
+        foreach (Match match in matchCollection)
+        {
+            expression = expression.Replace(
+                match.ToString(),
+                "Abs(" + match.Groups[1].ToString() + ")");
+        }
+        return expression;
+    }
+
+
+
+
+
+}
